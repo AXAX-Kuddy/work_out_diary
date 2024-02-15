ac15e5b60689c9149ff261057827521f382087ed
Kuddy <chromatik13@gmail.com>
2024-02-15 16:59:59 +0900

    2024.2.15

---
+++ work_out_app/lib/palette.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// 자주쓰는 색
+var bgColor = const Color.fromARGB(255, 22, 22, 22);
+var cardColorYelGreen = const Color.fromRGBO(210, 230, 94, 1);
+
+//
+// 티어색
+var tierLow = Colors.brown;
+var tierMiddleLow = const Color.fromRGBO(242, 136, 173, 1);
+var tierMiddle = const Color.fromRGBO(117, 99, 215, 1);
+var tierMiddleHigh = const Color.fromARGB(255, 245, 92, 49);
+var tierHigh = const Color.fromRGBO(82, 179, 175, 1);
+
+
---
+++ work_out_app/lib/widgets/base_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:work_out_app/palette.dart' as palette;
+
+class BasePage extends StatelessWidget {
+  final List<Widget> inputContent;
+
+  const BasePage({
+    super.key,
+    required this.inputContent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: palette.bgColor,
+        ),
+        // ignore: prefer_const_constructors
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              ...inputContent
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
